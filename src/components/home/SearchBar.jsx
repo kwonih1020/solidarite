9e90cb33a845097e57c2d14d5f5fa1f2f15f37ae
kwonih1020 <kwonih1020@gmail.com>
2022-10-23 02:42:39 +0900

    검색바 css 및 아이콘 삽입 필요사항 구현완료

--- src/components/home/SearchBar.jsx
+++ src/components/home/SearchBar.jsx
@@ -1,18 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
 
 import ArticleBox from "./ArticleBox";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBar = () => {
+  const iconFocus = useRef(null);
+
   const [search, setSearch] = useState("");
   const searchOnChange = (e) => {
     setSearch(e.target.value);
   };
 
+  const iconClickFocus = () => {
+    iconFocus.current.focus();
+  };
+
   return (
     <>
       <StSearchContainer>
-        <input type="text" value={search} onChange={searchOnChange} />
+        <div className="searchSection">
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="searchIcon"
+            onClick={iconClickFocus}
+          />
+          <input
+            type="text"
+            value={search}
+            onChange={searchOnChange}
+            placeholder="검색어를 입력하세요"
+            ref={iconFocus}
+          />
+        </div>
       </StSearchContainer>
       <ArticleBox search={search} />
     </>
@@ -23,11 +44,36 @@ const StSearchContainer = styled.div`
   margin-left: auto;
   margin-right: auto;
   width: 24rem;
-  input {
-    outline: transparent solid 2px;
-    outline-offset: 2px;
-    appearance: none;
-    flex: 1 1 auto;
+  .searchSection {
+    position: relative;
+    .searchIcon {
+      width: 14px;
+      height: 14px;
+      position: absolute;
+      top: 17px;
+      left: 10px;
+    }
+    input {
+      outline: transparent solid 2px;
+      outline-offset: 2px;
+      appearance: none;
+      flex: 1 1 auto;
+      border: none;
+      text-indent: 15px;
+      border-width: 1px;
+      padding: 1rem;
+      border-radius: 0.25rem;
+      display: flex;
+      border: 1px solid rgba(229, 231, 235);
+      transition-property: all;
+      transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+      transition-duration: 150ms;
+      width: 100%;
+      &:focus {
+        outline: none;
+        border-color: blue;
+      }
+    }
   }
 `;
 
